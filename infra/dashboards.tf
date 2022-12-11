2121bc33742458bcd8a9e1d8f9d7204fc6f9902c
NitroFray <32951247+NitroFray@users.noreply.github.com>
2022-12-11 21:09:21 +0100

    Added latency to dashboard, and alarm

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -60,6 +60,25 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "Total number of checkouts"
       }
+    },
+    {
+      "type": "metric",
+      "x": 12,
+      "y": 6,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "method.timed.avg"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Latency for shopping cart checkout"
+      }
     }
   ]
 }
--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -1,3 +1,20 @@
+resource "aws_cloudwatch_metric_alarm" "zerosum" {
+  alarm_name                = "carts-must-be-less-than-5"
+  namespace                 = "1033"
+  metric_name               = "carts.value"
+
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "5"
+  evaluation_periods        = "3"
+  period                    = "300"
+
+  statistic                 = "Maximum"
+
+  alarm_description         = "This alarm goes off as soon as the total amount of money in the bank exceeds 0"
+  insufficient_data_actions = []
+  alarm_actions       = [aws_sns_topic.user_updates.arn]
+}
+
 resource "aws_sns_topic" "alarms" {
   name = "alarm-topic-${var.candidate_id}"
 }
